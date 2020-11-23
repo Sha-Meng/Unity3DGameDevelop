3bb3ec3c34510d3e0d18bf363fac84a7c89f346c
Sha-Meng <54922354+Sha-Meng@users.noreply.github.com>
2020-11-23 20:35:33 +0800

    # 编辑栏扩展

---
+++ Unity3DDevelop/Assets/Editor/Script_03_03.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Script_03_03
+{
+    [InitializeOnLoadMethod]
+    static void InitializeOnLoadMethod()
+    {
+        // 为project窗口的每个可视列表项委派OnGUI事件
+        // Delegate for OnGUI events for every visible list item in the ProjectWindow.
+        EditorApplication.projectWindowItemOnGUI = delegate (string guid, Rect selectionRect)
+        {
+            // 在Project试图中选中一个资源
+            if (Selection.activeObject &&
+            guid == AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(Selection.activeObject)))
+            {
+                // 设置扩展按钮区域
+                float width = 50f;
+                selectionRect.x += (selectionRect.width - width);
+                selectionRect.y += 2f;
+                selectionRect.width = width;
+                GUI.color = Color.red;
+
+                // 点击事件
+                if (GUI.Button(selectionRect, "click"))
+                {
+                    Debug.LogFormat("click: {0}", Selection.activeObject.name);
+                }
+                GUI.color = Color.white;
+            }
+        };
+    }
+}
+
---
+++ Unity3DDevelop/Assets/Editor/Script_03_04.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Script_03_04 : UnityEditor.AssetModificationProcessor
+{
+    [InitializeOnLoadMethod]
+    static void InitializeOnLoadMethod()
+    {
+        // 全局监听Project视图下的资源是否发生变化(添加，删除和移动)
+        EditorApplication.projectChanged += delegate ()
+        {
+            Debug.Log("project assets changed");
+        };
+    }
+
+    /*
+    // 监听“双击鼠标左键，打开资源”事件
+    public static bool IsOpenForEdit(string assetPath, string message)
+    {
+        // message = null;
+        Debug.LogFormat("asset path : {0}", assetPath);
+
+        // true表示该资源可以打开， false表示不允许在Unity中打开该资源
+        return true;
+    }
+    */
+
+    // 监听 “资源即将被创建”事件 
+
+    public static void OnWillCreateAsset(string path)
+    {
+        Debug.LogFormat("path : {0}", path);
+    }
+    
+    // 监听“资源即将被保存”事件
+    public static string[] OnWillSaveAssets(string[] paths)
+    {
+        if(paths != null)
+        {
+            Debug.LogFormat("path : {0}", string.Join(",", paths));
+        }
+        return paths;
+    }
+
+    // 监听 “资源即将被移动”事件
+    public static AssetMoveResult OnWillMoveAsset(string oldPath, string newPath)
+    {
+        Debug.LogFormat("from : {0} to {1}", oldPath, newPath);
+
+        // DidMove: 该资源可以移动
+        return AssetMoveResult.DidMove;
+    }
+
+    // 监听“资源即将被删除”事件
+    public static AssetDeleteResult OnWillDeleteAsset(string path, RemoveAssetOptions option)
+    {
+        Debug.LogFormat("delete : {0}", path);
+
+        // DidDelete: 可以被删除
+        return AssetDeleteResult.DidDelete;
+    }
+}
